2fe5e55f869bf292613c9bd99a3326c5917a8f3f
unknown <luis.rodriguez@Saludonnet-OM.citasonnet.local>
2015-05-28 18:14:14 +0200

    adding product remove

--- MyAspNetWay/MyAspNetWay/Products/Scripts/View.js
+++ MyAspNetWay/MyAspNetWay/Products/Scripts/View.js
@@ -1,13 +1,13 @@
 ﻿function View(service) {
     $("#btnAdd").click(function () {
     });
-    $("#listOfTasks").empty();
+    $("#listOfProducts").empty();
     
     this.loadProducts = function() {
         var promiseResult = service.all();
-        promiseResult.done(function (tasks) {
-            _.each(tasks, function (task) {
-                $("#listOfTasks")
+        promiseResult.done(function (products) {
+            _.each(product, function (product) {
+                $("#listOfProducts")
                 .prepend('<div>' + task.title + ' ' + task.description +
                             ' <a id=' + task.id + ' href=\'#\' class="remove_task">Remove</a>' +
                             ' <a id=' + task.id + ' href=\'#\' class="update_task">Update</a>' +
